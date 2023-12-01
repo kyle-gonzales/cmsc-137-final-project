b56106b919843539098910663196c27e3bdf760f
Kyle Gonzales <kyleangelog@gmail.com>
2023-12-01 22:51:56 +0800

    feat: create constants class

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+class Constants:
+
+    APP_NAME = "k lang game" # temp
+
+    GAME_START = 0
+    GAME_IN_PROGRESS = 1
+    GAME_END = 2
+    WAITING_FOR_PLAYERS = 3
+
+    PORT = 5050
+
+    FORMAT = "utf-8"
+
+    DISCONNECT_MESSAGE = "!DISCONNECT"
+
+    HEADER_SIZE = 64

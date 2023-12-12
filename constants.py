d94324a8c80c767afd844d56288c5194e9d07dbf
Angel Estano <asestano@up.edu.ph>
2023-12-12 23:06:32 +0800

    updated file

--- constants.py
+++ constants.py
@@ -1,7 +1,72 @@
 import pygame
-import sys
+
 
 class Constants:
+    APP_NAME = "Dutete vs Narcos: Dynasty War"  # temp
+
+    GAME_START = 0
+    GAME_IN_PROGRESS = 1
+    GAME_END = 2
+    WAITING_FOR_PLAYERS = 3
+
+    PORT = 5050
+
+    FORMAT = "utf-8"
+
+    DISCONNECT_MESSAGE = "!DISCONNECT"
+
+    HEADER_SIZE = 64
+    
+    # Dynasty names
+    NARCOS = "NARCOS"
+    DUTETE = "DUTETE"
+    
+    # Screen dimensions
+    WIDTH = 960
+    HEIGHT = 540
+
+    # Colors
+    GOLD = (253, 165, 0)
+    MAROON = (147, 32, 37)
+    DARKERMAROON = (127, 12, 17)
+    GREEN = (0, 50, 19)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+
+    # Power Images
+    POWER_IMAGE_NAME = {
+        "Guns & Roses": "GunsAndRoses.png",
+        "Drugs Race": "DrugsRace.png",
+        "P*t*ng *n*": "ptng.png",
+        "Corruption": "corruption.png",
+        "Swiss Miss Bank": "swissmissbank.png",
+        "Debate Na Lang Kaya": "debate.png",
+        "Tank U": "tanku.png",
+        "Designer Shoes": "shoes.png",
+        "Tuna Panga": "panga.png",
+        "I See See": "iseesee.png",
+        "Bo Go": "bogo.png",
+    }
+
+    # Projectile coordinates
+    Yi = 390
+    PXi = 60
+    EXi = 900
+    
+    # Special Power
+    SPSIZE = (60, 60)
+    SPX1 = 250
+    SPX2 = 630
+    SPY1 = 120
+    SPY2 = 220
+    
+    # Font
+    pygame.init()
+    FONT32 = pygame.font.Font(None, 32)
+    FONT24 = pygame.font.Font(None, 24)
+    FONTH20 = pygame.font.Font(None, 20)
+    
+    
     pygame.init()
 
     #WELCOME SCREEN CONSTANTS
@@ -21,35 +86,35 @@ class Constants:
 
     # Calculate the center of the screen
     center_x, center_y = width // 2, height // 2
-
+    
     # Set up dots
     dot_radius = 5
     dot_spacing = 1
     dots = [(center_x - 50, center_y + 175), (center_x, center_y + 175), (center_x + 50, center_y + 175)]
-
+    
     # Set up the font
     font = pygame.font.SysFont("Courier", 50, "bold")
     prompt_text = font.render("Enter your name: ", True, (255, 255, 255))
     max_characters = 10
     display_default_underscores = True
-
+    
     #CHOOSE FAMILY CONSTANTS
-
+    
     # background for choose family
     choose_fam_bg = "Assets/choose_family_bg.png"
-
+    
 
     # family bg
     dutete_fam_bg = "Assets/dutete_info.png"
     narcos_fam_bg = "Assets/narcos_info.png"
-
+    
     # button paths
     button_paths = ["Assets/dutete_button.png", "Assets/narcos_button.png"]
     dutete_button_paths = ["Assets/dutete_button_hovered.png", "Assets/narcos_button.png"]
     narcos_button_paths = ["Assets/dutete_button.png", "Assets/narcos_button_hovered.png"]
     button_width, button_height = 300, 85
     button_spacing = 20
-
+    
     # CONSTANTS FOR CANNON MOVEMENT
 
     family_images = {
@@ -59,3 +124,4 @@ class Constants:
 
 
 
+    

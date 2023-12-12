e0724e4a2802e825eea25df6830174941e052dbb
Angel Estano <asestano@up.edu.ph>
2023-12-12 22:52:30 +0800

    constants update

--- constants.py
+++ constants.py
@@ -1,72 +1,7 @@
 import pygame
-
+import sys
 
 class Constants:
-    APP_NAME = "Dutete vs Narcos: Dynasty War"  # temp
-
-    GAME_START = 0
-    GAME_IN_PROGRESS = 1
-    GAME_END = 2
-    WAITING_FOR_PLAYERS = 3
-
-    PORT = 5050
-
-    FORMAT = "utf-8"
-
-    DISCONNECT_MESSAGE = "!DISCONNECT"
-
-    HEADER_SIZE = 64
-    
-    # Dynasty names
-    NARCOS = "NARCOS"
-    DUTETE = "DUTETE"
-    
-    # Screen dimensions
-    WIDTH = 960
-    HEIGHT = 540
-
-    # Colors
-    GOLD = (253, 165, 0)
-    MAROON = (147, 32, 37)
-    DARKERMAROON = (127, 12, 17)
-    GREEN = (0, 50, 19)
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-
-    # Power Images
-    POWER_IMAGE_NAME = {
-        "Guns & Roses": "GunsAndRoses.png",
-        "Drugs Race": "DrugsRace.png",
-        "P*t*ng *n*": "ptng.png",
-        "Corruption": "corruption.png",
-        "Swiss Miss Bank": "swissmissbank.png",
-        "Debate Na Lang Kaya": "debate.png",
-        "Tank U": "tanku.png",
-        "Designer Shoes": "shoes.png",
-        "Tuna Panga": "panga.png",
-        "I See See": "iseesee.png",
-        "Bo Go": "bogo.png",
-    }
-
-    # Projectile coordinates
-    Yi = 390
-    PXi = 60
-    EXi = 900
-    
-    # Special Power
-    SPSIZE = (60, 60)
-    SPX1 = 250
-    SPX2 = 630
-    SPY1 = 120
-    SPY2 = 220
-    
-    # Font
-    pygame.init()
-    FONT32 = pygame.font.Font(None, 32)
-    FONT24 = pygame.font.Font(None, 24)
-    FONTH20 = pygame.font.Font(None, 20)
-    
-    
     pygame.init()
 
     #WELCOME SCREEN CONSTANTS
@@ -122,4 +57,5 @@ class Constants:
     "Dutete": {"bg": "Assets/DuteteBG.png", "barrel": "Assets/dutete_cannon_barrel.png", "stand": "Assets/dutete_cannon_stand.png", "cannon": "Assets/narcos_cannon_zero.png", "state" : "False"}
     }
 
-    
+
+
